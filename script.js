f6d10677dec4c204dc8549bf3ec4e36f5e8055d5
Anshika <157712534+pumpkineater484@users.noreply.github.com>
2024-07-10 16:01:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const url = 'https://api.nasa.gov/planetary/apod?api_key=VyiCIsCt6vkEx2XvRZKaDMEJqgb9ot9bQwTZDY2G'
+const api_key = config.NASA_API_KEY
+
+const fetchNASAData = async () => {
+  try {
+    const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=VyiCIsCt6vkEx2XvRZKaDMEJqgb9ot9bQwTZDY2G')
+    const data = await response.json()
+    console.log('NASA APOD data', data)
+    displayData(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const displayData = data => {
+  document.getElementById('title').textContent = data.title
+  document.getElementById('date').textContent = data.date
+  document.getElementById('picture').src = data.hdurl
+  document.getElementById('explanation').textContent = data.explanation
+}
+
+fetchNASAData()
+
